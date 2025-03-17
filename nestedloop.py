db462fedf114f574925302c72d1df08f504ee983
sindhu <sindhu@gmail.com>
2025-03-17 12:18:18 +0530

    new file added

---
+++ nestedloop.py
@@ -0,0 +1,59 @@
+#x = list(input("Enter first list:"))
+#y = list(input("Enter second list:"))
+
+#for i in x:
+  #for j in y:
+    #print(i, j)
+#--------------------------------------------
+#x = [1, 2]
+#y = [4, 5]
+#i = 0
+#while i < len(x) :
+  #j = 0
+  #while j < len(y) :
+    #print(x[i] , y[j])
+   # j = j + 1
+  #i = i + 1
+#----------------------------------------------
+# Table creation for loop
+#n=int(input("Please enter the table number:"))
+#for i in range(n):
+  #  for j in range(1,11):
+ #       print(i, "*", j, "=",i*j)
+#    print()
+# ---------------------------------------------
+ 
+      
+
+
+
+
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+A
+
