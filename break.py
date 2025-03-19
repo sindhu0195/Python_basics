d323272d4d9fb5048403dff807dc34d59d8ac895
sindhu <sindhu@gmail.com>
2025-03-19 10:54:17 +0530

    new file added

---
+++ break.py
@@ -0,0 +1,6 @@
+cart=[10,20,50,550,600,70,80]
+for item in cart:
+    if item >500:
+        print("sorry we canot process this order insurance must be requred:")
+        break
+    print("processin item:",item)
